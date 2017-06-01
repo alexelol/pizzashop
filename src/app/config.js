7209e195e680462f008272177522c304d8329d59
alexelol <alejandrog1807@gmail.com>
2017-06-01 09:45:48 +0200

    Add files via upload

---
+++ src/app/config.js
@@ -0,0 +1,9 @@
+import { App } from './module';
+
+config.$inject = ["$urlRouterProvider"];
+export function config(urlRouterProvider) {
+    urlRouterProvider.otherwise('/');
+    
+}
+
+App.config(config);  
